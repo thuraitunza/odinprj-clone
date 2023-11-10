437fa1cf771810b0bb07a8fddd404ef786de9e61
Thurai Tun Za <147380435+thuraitunza@users.noreply.github.com>
2023-11-10 15:34:22 +0630

    setup odin project

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-[#121826] min-h-screen">
+        <Header />
+        <div class="">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container mx-auto my-[56px]">
+        <div class="pt-[4rem]">
+            <h1 class="text-white text-center text-[3.75rem] pb-6">
+                Your
+                <span class="text-[#A9792B]">
+                    Career in Web Development
+                </span>
+                <span class="block">Starts Here</span>
+            </h1>
+            <p class="text-white text-gray-400 text-xl flex mx-auto w-[40%] px-20 text-center pb-8">Our full stack curriculum is free and supported by a passionate open source community.</p>
+            <div class="flex justify-center">
+                <button class="border border-gray-400 text-white py-[12px] px-[24px] hover:bg-gray-500 rounded-md">View Full Curriculum</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
